340c50538cbde8dc9158064bfa98e58e07377a2d
Adam Ormondroyd <52655393+AdamOrmondroyd@users.noreply.github.com>
2024-05-20 12:13:47 +0100

    Allow easier swapping between `fgivenx.plot_lines` and `fgivenx.plot_contours` (#8)
    * remove unecessary title
    
    * sort out xlabel/ylabel
    
    * preemptively change docstring
    
    * import plot_lines
    
    * remove unused fig
    
    * docstring
    
    * remove repetition from docstring
    
    * add return type
    
    * remove ridiculous _ax
    
    * streamline CI
    
    * version bump to 2.3
    
    * reinstate polychord install

--- darkknot/plot.py
+++ darkknot/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from anesthetic import NestedSamples
-from fgivenx import plot_contours
+from fgivenx import plot_contours, plot_lines
 from darkknot import darkknot
 
 theory_list = [
@@ -18,22 +18,56 @@ theory_list = [
     darkknot.Adaptive,
 ]
 
-xlabel = r"$a$"
-ylabel = r"$w(a)$"
-
 
 def plot(
     samples: NestedSamples,
     ax=None,
     resolution=100,
     colors="Blues_r",
-    title=None,
-    fig=None,
+    xlabel=r"$a$",
+    ylabel=r"$w(a)$",
+    lines=False,
+    color="blue",
+    **kwargs,
 ):
+    """
+    Plot functional posterior of w(a) samples.
+
+    Parameters
+    ----------
+    samples : NestedSamples
+        Samples to plot.
+
+    ax : matplotlib.Axes, optional
+        Axes to plot on. If None, a new figure is created.
+
+    resolution : int, optional
+        Resolution of the plot.
+
+    colors : str, optional
+        Color map to use for contours.
+
+    xlabel : str, optional
+        Label for x-axis.
+
+    ylabel : str, optional
+        Label for y-axis.
+
+    lines : bool, optional
+        Plot lines instead of contours.
+
+    color : str, optional
+        Color of lines.
+
+    **kwargs : passed to fgivenx.plot_contours or fgivenx.plot_lines
+
+    Returns
+    -------
+    ax : matplotlib.Axes
+
+    """
     if ax is None:
-        _, _ax = plt.subplots()
-    else:
-        _ax = ax
+        _, ax = plt.subplots()
 
     # special case to allow Nw column to be added to samples, to treat
     # concatenated Vanilla samples to be treated as Adaptive, even if
@@ -49,14 +83,26 @@ def plot(
                 break
         keys = theory.params.keys()
 
-    cbar = plot_contours(
-        lambda a, theta: theory.flexknot(a, theta),
-        np.linspace(theory.amin, theory.atoday, resolution),
-        samples[keys],
-        weights=samples.get_weights(),
-        ax=_ax,
-        colors=colors,
-    )
-    _ax.set(xlabel=xlabel, ylabel=ylabel, title=title)
+    if lines:
+        plot_lines(
+            lambda a, theta: theory.flexknot(a, theta),
+            np.linspace(theory.amin, theory.atoday, resolution),
+            samples[keys],
+            weights=samples.get_weights(),
+            ax=ax,
+            color=color,
+            **kwargs,
+        )
+    else:
+        plot_contours(
+            lambda a, theta: theory.flexknot(a, theta),
+            np.linspace(theory.amin, theory.atoday, resolution),
+            samples[keys],
+            weights=samples.get_weights(),
+            ax=ax,
+            colors=colors,
+            **kwargs,
+        )
+    ax.set(xlabel=xlabel, ylabel=ylabel)
 
-    return _ax
+    return ax
