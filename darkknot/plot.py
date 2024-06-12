dc3656d176b7287d3027fa858eb043c2c4af1a38
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-06-12 11:06:23 +0100

    swap around plot_contours and plot_lines

--- darkknot/plot.py
+++ darkknot/plot.py
@@ -71,16 +71,6 @@ def plot(
         keys = theory.params.keys()
 
     if contours:
-        plot_lines(
-            lambda a, theta: theory.flexknot(a, theta),
-            np.linspace(theory.amin, theory.atoday, resolution),
-            samples[keys],
-            weights=samples.get_weights(),
-            ax=ax,
-            color=color,
-            **kwargs,
-        )
-    else:
         plot_contours(
             lambda a, theta: theory.flexknot(a, theta),
             np.linspace(theory.amin, theory.atoday, resolution),
@@ -88,6 +78,16 @@ def plot(
             weights=samples.get_weights(),
             ax=ax,
             colors=colors,
+            **kwargs,
+        )
+    else:
+        plot_lines(
+            lambda a, theta: theory.flexknot(a, theta),
+            np.linspace(theory.amin, theory.atoday, resolution),
+            samples[keys],
+            weights=samples.get_weights(),
+            ax=ax,
+            color=color,
             **kwargs,
         )
     ax.set(xlabel=xlabel, ylabel=ylabel)
